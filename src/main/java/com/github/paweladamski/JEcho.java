648b07728efa128212d958ccc0263332833385ae
Paweł Adamski <pawel@agflow.com>
2017-06-01 21:17:23 +0200

    Initial commit

---
+++ src/main/java/com/github/paweladamski/JEcho.java
@@ -0,0 +1,12 @@
+package com.github.paweladamski;
+
+/**
+ * Created by pawel on 29/04/17.
+ */
+public class JEcho {
+
+    public static void main(String[] args) {
+        String out = String.join(" ",args);
+        System.out.println(out);
+    }
+}
